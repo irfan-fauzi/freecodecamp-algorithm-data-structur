5e62f6a679c21da97083317831febae93c58235d
irfan-fauzi <irfanwintech@yahoo.com>
2020-07-14 20:16:41 +0700

    jalan yang benar, menggunakan if bersarang

--- execution-context-hoisting/algoritma-pencarian-arrayOfObject.js
+++ execution-context-hoisting/algoritma-pencarian-arrayOfObject.js
@@ -1,49 +1,51 @@
 // Setup
 var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["JavaScript", "Gaming", "Foxes"]
-    }
+  {
+    "firstName": "Akira",
+    "lastName": "Laine",
+    "number": "0543236543",
+    "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    "firstName": "Harry",
+    "lastName": "Potter",
+    "number": "0994372684",
+    "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+    "firstName": "Sherlock",
+    "lastName": "Holmes",
+    "number": "0487345643",
+    "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+    "firstName": "Kristian",
+    "lastName": "Vos",
+    "number": "unknown",
+    "likes": ["JavaScript", "Gaming", "Foxes"]
+  }
 ];
 
 
 function lookUpProfile(name, prop) {
-    // Only change code below this line
-    let hasil;
-    for (let i = 0; i < contacts.length; i++) {
-        if (name === contacts[i].firstName) {
-            hasil = contacts[i][prop];
-        }
-    }
-    if (hasil == undefined) {
-        return "nope";
-    }
-    return hasil
+  // Only change code below this line
 
-    // Only change code above this line
+  for (let i = 0; i < contacts.length; i++) {
+    if (name === contacts[i].firstName) {
+      if (contacts[i].hasOwnProperty(prop)) { // jika properti yang dimasukan sesuai
+        return contacts[i][prop];
+      } else { // jika property yang dimasukan salah
+        return "property salah";
+      }
+    }
+  }
+
+  return "nama salah";
+
+  // Only change code above this line
 }
 
-console.log(lookUpProfile("Harry", "lastName"));
+console.log(lookUpProfile("Kristian", "likes"));
 
 /* "Kristian", "lastName" should return "Vos"
 "Sherlock", "likes" should return ["Intriguing Cases", "Violin"]
@@ -51,3 +53,33 @@ console.log(lookUpProfile("Harry", "lastName"));
 "Bob", "number" should return "No such contact"
 "Bob", "potato" should return "No such contact"
 "Akira", "address" should return "No such property"-  */
+
+/*
+// SOLUTION 1
+function lookUpProfile(name, prop) {
+  for (var x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+
+// SOLUTION 2
+
+function lookUpProfile(name, prop) {
+  for (var i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (prop in contacts[i]) {
+        return contacts[i][prop];
+      } else return "No such property";
+    }
+  }
+  return "No such contact";
+}
+*/
