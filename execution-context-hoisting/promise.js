8b8e0f70f8ba0ca8ed1e762fc3e7a25db73aacc4
irfan-fauzi <irfanwintech@yahoo.com>
2020-07-19 17:11:19 +0700

    promise then & catch

--- execution-context-hoisting/promise.js
+++ execution-context-hoisting/promise.js
@@ -17,3 +17,26 @@ const makeServerRequest = new Promise((resolve, reject) => {
         reject("Data not received") // jika janji tidak terpenuhi
     }
 });
+
+/*
+Promises are most useful when you have a process that takes an unknown amount of time in your code (i.e. something asynchronous), often a server request. When you make a server request it takes some amount of time, and after it completes you usually want to do something with the response from the server. This can be achieved by using the then method. The then method is executed immediately after your promise is fulfilled with resolve. Here’s an example:
+
+*/
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+// yang dilakukan segera setelah promise berhasil
+makeServerRequest.then(result => {
+    console.log(result);
+});
+// yang dilakukan segera setelah promise tidak berhasil (error)
+makeServerRequest.catch(error => {
+    console.log(error);
+});
