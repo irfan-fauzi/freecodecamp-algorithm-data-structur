b664d696a313486062fa3a279084913b5f8a7cc8
irfan-fauzi <irfanwintech@yahoo.com>
2020-08-09 17:35:24 +0700

    multidimensi array

---
+++ es6/dimensiarray.js
@@ -0,0 +1,8 @@
+let myNestedArray = [
+    // Only change code below this line
+    [
+        ["deep", 3, true, ["deeper", 4, false, ["deepest", 5, true]]]
+    ]
+    // Only change code above this line
+];
+console.log(myNestedArray[0][0])
