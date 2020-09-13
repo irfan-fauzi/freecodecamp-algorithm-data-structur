474a81fbf7c98ae7ca244ea127834ae60f059ef0
irfan-fauzi <irfanwintech@yahoo.com>
2020-09-13 14:16:26 +0700

    function factories

---
+++ functional-programming/immutable.js
@@ -0,0 +1,12 @@
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer(arg) {
+    return arg + 1;
+
+    // Only change code above this line
+}
+let penambahan = incrementer(fixedValue);
+console.log(penambahan) // 5
+console.log(fixedValue) // tetap 4
