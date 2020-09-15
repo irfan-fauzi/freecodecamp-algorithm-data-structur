2bcf8184ef5e7d0812823d6bf887a563fd934dd1
irfan-fauzi <irfanwintech@yahoo.com>
2020-09-16 06:51:53 +0700

    cara kerja fungsi map

---
+++ functional-programming/callback.js
@@ -0,0 +1,19 @@
+// The global variable
+var s = [23, 65, 98, 5];
+var x = [2, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+  // Only change code below this line
+  for(let i = 0; i < this.length; i++){
+    newArray.push(callback(this[i]))
+  }
+  
+  return newArray;
+
+};
+
+var new_s = s.myMap(function(item){
+  return item * 2;
+});
+console.log(new_s)
