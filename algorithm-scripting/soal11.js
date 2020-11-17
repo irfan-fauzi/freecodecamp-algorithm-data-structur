7bc09ce92b09b7c18d45bd881737c0b2f75cd65a
irfan-fauzi <irfanwintech@yahoo.com>
2020-11-17 07:49:27 +0700

    bilangan prima

---
+++ algorithm-scripting/soal11.js
@@ -0,0 +1,16 @@
+// function fibo(n) {
+//     let hasil = [0, 1];
+//     for (let i = 2; i < n + 1; i++) {
+//         hasil.push(hasil[i - 2] + hasil[i - 1])
+//     }
+//     return hasil.filter(el => el <= n).filter(el => el % 2 == 1).reduce((acc, curr) => acc + curr)
+// }
+
+// console.log(fibo(10))
+
+function prime(n) {
+    let hasil = []
+    for (let i = 2; i <= n; i++) {
+        
+    }
+}
